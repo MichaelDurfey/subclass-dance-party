9c20b15d3ef856db4b26aeb1124f7ef8ce319002
Michael Durfey <mdurfey@me.com>
2018-01-23 10:28:43 -0800

    added dancers

--- src/TapDancer.js
+++ src/TapDancer.js
@@ -1,12 +1,13 @@
-var MakeBreakDancer = function(top, left, timeBetweenSteps) {
+var TapDancer = function(top, left, timeBetweenSteps) {
   MakeDancer.call(this, top, left, timeBetweenSteps);
-  this.$node = $('div class = "breakDancer-Container"</div>')
-  this.$node.html('<div class="break-dancerContainer" <img class="break-dancer" src="sources/break-dancer.jpg"></img></div>');
+  this.$node = $('');
+  this.$node = $('<img src="sources/tapDancer.jpg" class="tap-dancer">');
   this.spin();
 };
 
 MakeBreakDancer.prototype = Object.create(MakeDancer.prototype);
 MakeBreakDancer.prototype.constructor = MakeBreakDancer;
+
 
 //access to this.$node was prevented because create wasn't invoked
 MakeBreakDancer.prototype.step = function() {
@@ -18,14 +19,9 @@ MakeBreakDancer.prototype.spin = function() {
   (this.$node).animate({
     left: '+=100'
   }, {
-    duration: 5000,
+    duration: 1000,
     step: function( now, fx ) {
       $( this.$node).css('left', now);
     }
   });
 };  
-
-
-//replace toggle with a rotate transition
- 
-//write our tests in the specFolder for new dancer
